f5615fa97f904cf925a7cd3f3987fe1c80c2fcfe
Lokesh Vasnik <91425128+lokeshvasnik@users.noreply.github.com>
2022-11-16 23:49:30 +0530

    Updated the whole UI
    More to do

--- Js/app.js
+++ Js/app.js
@@ -1,36 +1,64 @@
 const searchBtn = document.querySelector('#search-btn');
-const countryInp = document.querySelector('#country-inp');
+const countryInp = document.querySelector('.country-inp');
 const result = document.querySelector('#result');
 
 // Search Btn
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
+  const searchWrapper = document.querySelector('.box-model');
+
   let countryName = countryInp.value;
+
   let finalURL = `https://restcountries.com/v3.1/name/${countryName}`;
 
   // fetching data from the rest api
-  fetch(finalURL)
-    .then((response) => response.json())
-    .then((data) => {
-      // Injecting to html
-      result.innerHTML = `
-      <img src="${data[0].flags.svg}" class="flag-img">
-      <h2> ${data[0].name.common}</h2>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Capital:</h4>
-                <span>${data[0].capital[0]}</span>
-            </div>
-        </div>  
-        
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Continent:</h4>
-                <span>${data[0].continents[0]}</span>
-            </div>
-        </div>    
 
-      `;
-    });
+  const loader = document.querySelector('.container');
+
+  const showLoader = () => {
+    result.innerHTML = 'Loading';
+    searchWrapper.classList.add('show');
+  };
+
+  const hideLoader = () => {
+    loader.classList.remove('show');
+    searchWrapper.classList.remove('show');
+    countryInp.value = '';
+  };
+
+  // Loading function starts
+  showLoader();
+
+  // Interal function
+  setTimeout(() => {
+    fetch(finalURL)
+      .then((response) => response.json())
+      .then((data) => {
+        // Injecting to html
+
+        hideLoader();
+        result.innerHTML = `
+         <img src="${data[0].flags.svg}" class="flag-img">
+
+        <h2> ${data[0].name.common}</h2>
+          <div class="wrapper">
+              <div class="data-wrapper">
+                  <h4>Capital:</h4>
+                  <span>${data[0].capital[0]}</span>
+              </div>
+          </div>  
+                  
+        `;
+      })
+      .catch(() => {
+        result.innerHTML = `
+        <div class="wrapper">
+          <div class="data-wrapper">
+              <h4>Please Check Input</h4>
+          </div>
+       </div>  `;
+        // return;
+      });
+  }, 1050);
 });
